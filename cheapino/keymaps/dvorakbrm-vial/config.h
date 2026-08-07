f19d92aeeb80a4bc74d1164eb8df6bed7a21e967
Chris O'Callaghan <christopher.l.ocallaghan@gmail.com>
2026-08-07 10:54:06 -0400

    Added the whole source tree for the cheapino board from my vial-qmk source tree, with necessary edits to source files to allow for succesful compilation of vial firmware.

---
+++ cheapino/keymaps/dvorakbrm-vial/config.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+#define VIAL_KEYBOARD_UID {0x9B, 0xED, 0x09, 0x7D, 0x0D, 0x10, 0x65, 0x05}
+//#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+//#define VIAL_UNLOCK_COMBO_COLS { 5, 4 }
+//#define COMBO_COUNT 2
+//#define RAW_EPSIZE 32
+//#define PAYLOAD_MARK 0x90
+//#define PAYLOAD_BEGIN 24
+//#define CHORDAL_HOLD
+//#define PERMISSIVE_HOLD
+//#define FLOW_TAP_TERM 150
+//#define TAPPING_TERM_PER_KEY
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+// Not required for Vial
+//#define RAW_USAGE_PAGE 0xFF60
+//#define RAW_USAGE 0x61
---
+++ cheapino/keymaps/dvorakbrm/config.h
@@ -0,0 +1,12 @@
+#define COMBO_COUNT 2
+#define RAW_EPSIZE 32
+#define PAYLOAD_MARK 0x90
+#define PAYLOAD_BEGIN 24
+#define CHORDAL_HOLD
+#define PERMISSIVE_HOLD
+#define FLOW_TAP_TERM 150
+#define TAPPING_TERM_PER_KEY
+
+// Not required for Vial
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE 0x61
